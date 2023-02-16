bb5695e86d77a6a93120da5565ced08d13723d95
Hillel1408 <etemax@bk.ru>
2023-02-16 12:38:52 +0300

    стома дент

--- #src/js/script.js
+++ #src/js/script.js
@@ -5,3 +5,14 @@ $(document).ready(function () {
         slidesToShow: 1,
     });
 });
+
+const navIcon = document.querySelector('.header__nav-icon');
+const navList = document.querySelector('.header__nav-list');
+const popup = document.querySelector('.popup');
+
+navIcon.addEventListener('click', () => {
+    navIcon.classList.toggle('active');
+    navList.classList.toggle('active');
+    document.body.classList.toggle('lock');
+    popup.classList.toggle('active');
+});
