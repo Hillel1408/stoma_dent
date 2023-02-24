0368f77021509952eb1ca550758668ef54e13bd8
Hillel1408 <etemax@bk.ru>
2023-02-24 17:44:46 +0300

    stoma dent

--- #src/js/script.js
+++ #src/js/script.js
@@ -7,12 +7,10 @@ $(document).ready(function () {
 });
 
 const navIcon = document.querySelector('.header__nav-icon');
-const navList = document.querySelector('.header__nav-list');
+const navList = document.querySelector('.header__nav-list2');
 const popup = document.querySelector('.popup');
 
 navIcon.addEventListener('click', () => {
     navIcon.classList.toggle('active');
     navList.classList.toggle('active');
-    document.body.classList.toggle('lock');
-    popup.classList.toggle('active');
 });
