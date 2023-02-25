fc5960c73cfe6c89d4559b54222406f3a7008fbe
Hillel1408 <etemax@bk.ru>
2023-02-25 12:49:54 +0300

    stoma dent

--- #src/js/script.js
+++ #src/js/script.js
@@ -8,9 +8,35 @@ $(document).ready(function () {
 
 const navIcon = document.querySelector('.header__nav-icon');
 const navList = document.querySelector('.header__nav-list2');
-const popup = document.querySelector('.popup');
+
+function a(e) {
+    if (
+        !e.target.closest('.header__nav-list2') &&
+        !e.target.closest('.header__nav-icon')
+    ) {
+        navIcon.classList.toggle('active');
+        navList.classList.toggle('active');
+        document.removeEventListener('click', a);
+    }
+}
 
 navIcon.addEventListener('click', () => {
     navIcon.classList.toggle('active');
     navList.classList.toggle('active');
+    if (navList.classList.contains('active')) {
+        document.addEventListener('click', a);
+    } else {
+        document.removeEventListener('click', a);
+    }
+});
+
+const navItems = document.querySelectorAll('.header__nav-title');
+
+navItems.forEach((item) => {
+    if (item.parentNode.childNodes[3]) {
+        item.classList.add('asd');
+    }
+    item.addEventListener('click', () => {
+        item.parentNode.childNodes[3].classList.toggle('active');
+    });
 });
