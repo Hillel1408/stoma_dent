0dde2ec40700e8e386575eac5feb77459cd715bd
Hillel1408 <etemax@bk.ru>
2023-03-07 18:05:17 +0300

    Updates

---
+++ js/script.js
@@ -0,0 +1,42 @@
+$(document).ready(function () {
+    $('.main__slider').slick({
+        dots: true,
+        arrows: true,
+        slidesToShow: 1,
+    });
+});
+
+const navIcon = document.querySelector('.header__nav-icon');
+const navList = document.querySelector('.header__nav-list2');
+
+function a(e) {
+    if (
+        !e.target.closest('.header__nav-list2') &&
+        !e.target.closest('.header__nav-icon')
+    ) {
+        navIcon.classList.toggle('active');
+        navList.classList.toggle('active');
+        document.removeEventListener('click', a);
+    }
+}
+
+navIcon.addEventListener('click', () => {
+    navIcon.classList.toggle('active');
+    navList.classList.toggle('active');
+    if (navList.classList.contains('active')) {
+        document.addEventListener('click', a);
+    } else {
+        document.removeEventListener('click', a);
+    }
+});
+
+const navItems = document.querySelectorAll('.header__nav-title');
+
+navItems.forEach((item) => {
+    if (item.parentNode.childNodes[3]) {
+        item.classList.add('asd');
+    }
+    item.addEventListener('click', () => {
+        item.parentNode.childNodes[3].classList.toggle('active');
+    });
+});
